64ce4ebeb8b93cc05966944f10c381c594e63fe7
Sergey Polischuk <spolischook@gmail.com>
2014-11-19 08:31:41 +0200

    Updated indexAction

--- src/Spolischook/MiniShopBundle/Controller/IndexController.php
+++ src/Spolischook/MiniShopBundle/Controller/IndexController.php
@@ -2,18 +2,23 @@
 
 namespace Spolischook\MiniShopBundle\Controller;
 
+use Spolischook\MiniShopBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class IndexController extends Controller
 {
     /**
      * @Template()
      * @Route("/")
+     * @Method({"GET"})
      */
     public function indexAction()
     {
-        return [];
+        $stores = $this->getDoctrine()->getManager()->getRepository('MiniShopBundle:Store')->findAll();
+
+        return ['stores' => $stores];
     }
 }
